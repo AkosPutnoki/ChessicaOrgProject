a62dde20b84fc080e23f4c57dc897be49aba3e7e
Akos Putnoki <putnokiakos@gmail.com>
2018-05-16 15:37:11 +0200

    result enum added

--- src/main/java/com/chessica/controller/MatchControllerREST.java
+++ src/main/java/com/chessica/controller/MatchControllerREST.java
@@ -1,10 +1,20 @@
 package com.chessica.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class MatchControllerREST {
 
-
+    @PostMapping(value = "/api/game/pathvalidation", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity pathValidation(@RequestBody Map<String, String> data){
+        //TODO
+        return null;
+    }
 
 }
